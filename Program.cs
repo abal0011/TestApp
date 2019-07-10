23303abe84adc0535abeead2f56f44da33753e15
abal0011 <aravind.cyber@yahoo.co.in>
2019-07-11 00:25:13 +1000

    Basic Regex to split the input

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace TestApp
@@ -16,7 +17,15 @@ namespace TestApp
             name = Console.ReadLine();
             String[] elements = System.Text.RegularExpressions.Regex.Split(name, pattern);
             foreach (var element in elements)
-                Console.WriteLine(element);
+            {
+                String newPattern = @"([|A-Z|a-z| ]*)([\d]*)";
+                var match = Regex.Match(element,newPattern);
+                var value = match.Groups[1].Value;
+                var digit = int.Parse(match.Groups[2].Value); 
+                Console.WriteLine(value);
+                Console.WriteLine(digit);
+            }
+                
             Console.ReadLine();
         }
     }
