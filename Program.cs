0d0ced0e42bfc1957ccdb7becb129d217f627e2f
abal0011 <aravind.cyber@yahoo.co.in>
2019-07-11 00:32:05 +1000

    Basic add and subtract distance working with front & back

--- Program.cs
+++ Program.cs
@@ -13,6 +13,7 @@ namespace TestApp
         {
             String name;
             String pattern = ",";
+            Int32 distance = 0;
             Console.WriteLine("Write a Line");
             name = Console.ReadLine();
             String[] elements = System.Text.RegularExpressions.Regex.Split(name, pattern);
@@ -22,9 +23,23 @@ namespace TestApp
                 var match = Regex.Match(element,newPattern);
                 var value = match.Groups[1].Value;
                 var digit = int.Parse(match.Groups[2].Value); 
-                Console.WriteLine(value);
-                Console.WriteLine(digit);
+                if(value == "F")
+                {
+                    if( digit > 0)
+                    {
+                        distance += digit;
+                        
+                    }
+                }     
+                else if (value == "B")
+                {
+                    if( digit > 0)
+                    {
+                        distance -= digit;
+                    }
+                }
             }
+            Console.WriteLine(distance);
                 
             Console.ReadLine();
         }
