2e3b54ebee8c027197ad19cd78f152f0a11aae17
abal0011 <aravind.cyber@yahoo.co.in>
2019-07-10 20:58:33 +1000

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name;
+            String pattern = ",";
+            Console.WriteLine("Write a Line");
+            name = Console.ReadLine();
+            String[] elements = System.Text.RegularExpressions.Regex.Split(name, pattern);
+            foreach (var element in elements)
+                Console.WriteLine(element);
+            Console.ReadLine();
+        }
+    }
+}
