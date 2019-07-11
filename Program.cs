628963a08a14650e9a5744fbb13fcc62ab26633c
abal0011 <aravind.cyber@yahoo.co.in>
2019-07-11 12:33:07 +1000

    Basic operation for F B R L without testing and validation of input data

--- Program.cs
+++ Program.cs
@@ -9,11 +9,13 @@ namespace TestApp
 {
     class Program
     {
+
         static void Main(string[] args)
         {
             String name;
             String pattern = ",";
             Int32 distance = 0;
+            Int32 Location = 0;
             Console.WriteLine("Write a Line");
             name = Console.ReadLine();
             String[] elements = System.Text.RegularExpressions.Regex.Split(name, pattern);
@@ -25,7 +27,7 @@ namespace TestApp
                 var digit = int.Parse(match.Groups[2].Value); 
                 if(value == "F")
                 {
-                    if( digit > 0)
+                    if( digit > 0 && Location == 0)
                     {
                         distance += digit;
                         
@@ -33,13 +35,58 @@ namespace TestApp
                 }     
                 else if (value == "B")
                 {
-                    if( digit > 0)
+                    if( digit > 0 && Location == 0)
                     {
                         distance -= digit;
                     }
+                    else if (digit >0 && Location == 2)
+                    {
+                        distance += digit;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Entered Wrong input");
+                    }
+                }
+                else if (value == "R")
+                {
+                    if(digit> 0)
+                    {
+                        if( digit/2 == 1 && distance !=0 && digit%4 !=0)
+                        {
+                            Location = 2;                         
+                        }
+                        else if (digit%4 ==0)
+                        {
+                            Location = 0;
+                        }
+                        else
+                        {
+                            Location = 0;
+                        }
+                    }
+                }
+                else if (value == "L")
+                {
+                    if(digit > 0)
+                    {
+                        if (digit / 2 == 1 && distance != 0 && digit % 4 != 0)
+                        {
+                            Location = 2;
+                        }
+                        else if (digit % 4 == 0)
+                        {
+                            Location = 0;
+                        }
+                        else
+                        {
+                            Location = 0;
+                        }
+                    }
                 }
             }
-            Console.WriteLine(distance);
+    // To print the magnitude alone of the integer and omit the sign using Math.Abs()
+            Console.WriteLine(Math.Abs(distance));
                 
             Console.ReadLine();
         }
